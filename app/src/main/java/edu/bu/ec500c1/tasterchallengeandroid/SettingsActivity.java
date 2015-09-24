86377a01ab51da32b220880f4b75043e650af330
jmeunier28 <jmeunier@bu.edu>
2015-09-24 08:38:13 -0400

    Complete

--- app/src/main/java/edu/bu/ec500c1/tasterchallengeandroid/SettingsActivity.java
+++ app/src/main/java/edu/bu/ec500c1/tasterchallengeandroid/SettingsActivity.java
@@ -1,17 +1,86 @@
 package edu.bu.ec500c1.tasterchallengeandroid;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.SeekBar;
+import android.media.AudioManager;
+import android.widget.ImageButton;
+import android.content.Intent;
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
+
+    public SeekBar mSeekBarVolume;
+    public MediaPlayer mMP;
+    public AudioManager mAM;
+    public ImageButton mVolumeSetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        mSeekBarVolume = (SeekBar) findViewById(R.id.sbVolume);
+        mMP = MediaPlayer.create(this,R.raw.backgroundmusic);
+        mMP.start();
+        mAM = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        int maxVolume = mAM.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = mAM.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mSeekBarVolume.setMax(maxVolume);
+        mSeekBarVolume.setProgress(currentVolume);
+
+        mSeekBarVolume.setOnSeekBarChangeListener(this);
+
+        setVolume();
+
     }
+    protected void onPause() {
+        super.onPause();
+        mMP.release();
+    }
+
+    public void setVolume(){
+
+        mVolumeSetButton = (ImageButton) findViewById(R.id.checkbox);
+        mVolumeSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // here we want to go back to the main activity page
+                // can we make this so we dont stop playing the music??
+                //Intent intent = new Intent();
+                //setResult(RESULT_OK);
+                onBackPressed();
+            }
+        });
+    }
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+        mAM.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+        //blah blah blah
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+        //blah blah blah
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +103,7 @@ public class SettingsActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
+
